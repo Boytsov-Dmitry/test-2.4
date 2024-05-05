0311d6c9e4dfec154196d44f09a93bdfbc9e9af2
Dmitry Boytsov <135224601+Boytsov-Dmitry@users.noreply.github.com>
2024-05-05 17:02:56 +0300

    Update

--- src/js/Class/Daemon.js
+++ src/js/Class/Daemon.js
@@ -3,7 +3,6 @@ import Character from './MainCharacter';
 export default class Daemon extends Character {
     constructor(name, type) {
         super(name, type);
-        this.type = 'Daemon';
         this.attack = 10;
         this.defence = 40;
     };
