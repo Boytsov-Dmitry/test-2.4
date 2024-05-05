05036301c8ce82869d6c942b447df32544cd68b3
Dmitry Boytsov <135224601+Boytsov-Dmitry@users.noreply.github.com>
2024-05-05 17:03:31 +0300

    Update

--- src/js/Class/Undead.js
+++ src/js/Class/Undead.js
@@ -3,7 +3,6 @@ import Character from './MainCharacter';
 export default class Undead extends Character {
     constructor(name, type) {
         super(name, type);
-        this.type = 'Undead';
         this.attack = 25;
         this.defence = 25;
     };
