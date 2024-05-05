f5432dee04bef28d127a0613f5c1316e47becf48
Dmitry Boytsov <135224601+Boytsov-Dmitry@users.noreply.github.com>
2024-05-05 16:54:54 +0300

    Update

--- src/js/Class/MainCharacter.js
+++ src/js/Class/MainCharacter.js
@@ -1,20 +1,20 @@
 export default class Character {
     constructor (name, type) {
-        const typesOfPlayers = ['Bowman', 'Daemon', 'Magician', 'Swordsman', 'Undead', 'Zombie']
+        const typesOfPlayers = ['Bowman', 'Daemon', 'Magician', 'Swordsman', 'Undead', 'Zombie'];
         if((name.length < 2) || (name.length > 10)) {
-            throw new Error('Не верно')
-        };
+            throw new Error('Не верно');
+        }
 
         if(!typesOfPlayers.includes(type)) {
-            throw new Error('Не верно')
-        };
+            throw new Error('Не верно');
+        }
 
-        this.name = name,
-        this.type = type,
-        this.health = 0,
-        this.level = 1,
-        this.attack = '',
-        this.defence = ''
+        this.name = name;
+        this.type = type;
+        this.health = 1000;
+        this.level = 1;
+        this.attack = undefined;
+        this.defence = undefined;
     };
     
     levelUp() {
@@ -24,15 +24,15 @@ export default class Character {
             this.defence = this.defence * 1.2;
             this.health = 100;
         } else {
-            throw new Error('Нельзя повысить левел умершего')
-        };
+            throw new Error('Нельзя повысить левел умершего');
+        }
     };
 
     damage(points) {
         if(this.health >= 0) {
             this.health -= points * (1 - this.defence / 100)
         } else {
-            throw new Error('Персонаж погиб')
+            throw new Error('Персонаж погиб');
         }
     };
 };
