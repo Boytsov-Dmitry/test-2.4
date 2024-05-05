205b8cd91db85e3fce9c75fbc3cae877fdd1f6b8
Dmitry Boytsov <135224601+Boytsov-Dmitry@users.noreply.github.com>
2024-05-05 17:03:06 +0300

    Update

--- src/js/Class/Magician.js
+++ src/js/Class/Magician.js
@@ -3,7 +3,6 @@ import Character from './MainCharacter';
 export default class Magician extends Character {
     constructor(name, type) {
         super(name, type);
-        this.type = 'Magician';
         this.attack = 10;
         this.defence = 40;
     };
